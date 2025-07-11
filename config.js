f0aa17ead83b8fa015a3db9a72d4b5738e978542
Iamabhih <steelabhishek@gmail.com>
2025-07-11 14:50:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Google Drive API configuration
+const CONFIG = {
+    API_KEY: 'AIzaSyDHKDrY2UUT9_HZhBY6GMAkyHYHIdv7OsA',
+    FOLDER_ID: '1tG66zQTXGR-BQwjYZheRHVQ7s4n6-Jan',
+    CACHE_KEY: 'ozz_image_mapping_v3',
+    CACHE_TTL: 24 * 60 * 60 * 1000, // 24 hours cache
+    PRODUCT_SKUS: [
+        "455641", "455642", "455639", /* ... all your SKUs ... */ 
+        "455322", "455323"
+    ],
+    DATA_SOURCE: './JULY PROMO.xlsx'
+};
